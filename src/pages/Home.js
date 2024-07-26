bc401d9984586bf7c8a733160f8cb4b56fad5a92
wagtzero <157348878+wagtzero@users.noreply.github.com>
2024-07-26 15:48:52 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import styles from './Home.module.css';
+import logo from '../assets/logo.png';
 
 function Home() {
   return (
-    <div>
+    <div className={styles.container}>
+      <img src={logo} alt="Company Logo" className={styles.logo} />
       <h1>Welcome to Our Tour Company</h1>
     </div>
   );
