04a1201d15b17c842dce552a9174da774a70198d
wagtzero <157348878+wagtzero@users.noreply.github.com>
2024-07-26 15:52:28 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/global.css';
+import App from './App';
+import StripeProvider from './StripeContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <StripeProvider>
+      <App />
+    </StripeProvider>
+  </React.StrictMode>
+);
